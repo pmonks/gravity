aba1e684f288c80bf43af6e85bfe31b10e5a8a5c
Peter Monks <pmonks@gmail.com>
2023-03-14 14:03:30 -0700

    :racehorse: More performance tweaks

--- src/gravity/core.clj
+++ src/gravity/core.clj
@@ -20,9 +20,13 @@
 (ns gravity.core
   (:require [clojure.math.combinatorics :as comb]))
 
-(def G                       1.5)    ; Our dodgy version of the gravitational constant
-(def mass-factor             1.25)   ; Exponent for mass, to help keep the simulation compact
-(def mass-to-radius-exponent 0.6)    ; Exponent of mass to radius. Real value for stars is ~0.8.
+; Note: because we store numbers in maps, we cannot have fully unboxed numbers (the JVM doesn't (yet) support primitives in maps)
+;(set! *warn-on-reflection* true)
+;(set! *unchecked-math* :warn-on-boxed)
+
+(def ^{:tag 'double} G                       1.5)    ; Our dodgy version of the gravitational constant
+(def ^{:tag 'double} mass-factor             1.25)   ; Exponent for mass, to help keep the simulation compact
+(def ^{:tag 'double} mass-to-radius-exponent 0.6)    ; Exponent of mass to radius. Real value for stars is ~0.8.
 
 (defmacro sq
   "The square of x."
@@ -36,10 +40,10 @@
   `(+ (sq (- (:x ~o2) (:x ~o1)))
       (sq (- (:y ~o2) (:y ~o1)))))
 
-(def π java.lang.Math/PI)
+(def ^{:tag 'double} π java.lang.Math/PI)
 
-(defmacro rads-to-degs
-  "The angle in degrees of rad (radians)."
+(defmacro rad-to-deg
+  "The angle of rad (radians) in degrees."
   [rad]
   `(* ~rad (/ 180 π)))
 
@@ -69,18 +73,16 @@
    1. magnitude of gravitational force
    2. direction of gravitational force (radians)"
   [o1 o2]
-  [(* G (/ (* (scaled-mass o1) (scaled-mass o2))
-           (max (square-distance o1 o2) 0.00000001)))   ; Ensure we don't divide by zero
-   (atan2 (- (:y o2) (:y o1)) (- (:x o2) (:x o1)))])
+  [(* G (/ (* ^double (scaled-mass o1) ^double (scaled-mass o2))
+           (max ^double (square-distance o1 o2) 0.00000001)))   ; Ensure we don't divide by zero
+   (atan2 (- ^double (:y o2) ^double (:y o1)) (- ^double (:x o2) ^double (:x o1)))])
 
 (defn g-force-rect
   "Returns gravitational force between two 'objects' as a rectilinear vector of 2 elements:
    1. magnitude of force in horizontal (X) plane
    2. magnitude of force in vertical (Y) plane"
   [o1 o2]
-  (let [gf  (g-force-polar o1 o2)
-        mag (first  gf)
-        dir (second gf)]
+  (let [[mag dir] (g-force-polar o1 o2)]
     [(* mag (cos dir)) (* mag (sin dir))]))
 
 (defn accel-rect
@@ -88,13 +90,13 @@
    1. acceleration in horizontal (X) plane
    2. acceleration in vertical (Y) plane"
   [o1 o2]
-  (let [gf (g-force-rect o1 o2)]
-    [(/ (first  gf) (scaled-mass o1))
-     (/ (second gf) (scaled-mass o1))]))
+  (let [[x-force y-force] (g-force-rect o1 o2)]
+    [(/ x-force (scaled-mass o1))
+     (/ y-force (scaled-mass o1))]))
 
 (defn step-simul-pair
   "Returns the accelerations obj1 and obj2 have on each other, as a vector of 2 elements,
-  each of which is a map containing the obj (:obj) and the acceleration (as per accel-rect) it is experiencing (:accel)."
+  each of which is a map containing the obj (:obj) and the acceleration (as per accel-rect) it is experiencing (::accel)."
   [[obj1 obj2]]
   [{:obj   obj1
     ::accel (accel-rect obj1 obj2)}
@@ -118,9 +120,9 @@
 ;     (/ (sum ~nums) (count ~nums))))
 
 (defmacro radius
-  "The radius of the given object based on its mass, rounded down to the nearest integer."
+  "The radius of the given object based on its mass, rounded down to the nearest integer (long)."
   [o]
-  `(int (max 1 (pow (:mass ~o) mass-to-radius-exponent))))
+  `(long (max 1 (pow (:mass ~o) mass-to-radius-exponent))))
 
 (defn- next-locs-and-vels
   "Calculates the next locations and velocities of the given objects."
@@ -196,8 +198,8 @@
 (defn- merge-objects
   "Merges the given objects, by adding their masses, weighted-averaging their location and velocity, and retaining the colour of the most massive object."
   [objs]
-  (let [highest-mass (apply max (map :mass objs))
-        primary      (first (filter #(= highest-mass (:mass %)) objs))
+  ; Note: don't use scaled masses here!
+  (let [primary      (last (sort-by :mass objs))
         total-mass   (sum (map :mass objs))]
     (assoc primary
            :mass   total-mass
